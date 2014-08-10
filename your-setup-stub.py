b6fdb244c6824a133c3cac2e7ddd6215694ecd33
Stuart Herbert <stuart@stuartherbert.com>
2014-08-10 11:16:14 +0100

    New: first attempt at setup.py for you

---
+++ your-setup-stub.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# our imports
+import os
+import subprocess
+import sys
+import time
+
+# special case - get the devsetup framework
+if not os.path.isdir("devsetup"):
+    subprocess.call(["git", "clone", "https://github.com:devsetup/devsetup.git"])
+
+# load devsetup
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "devsetup"))
+import devsetup
+devsetup.init(__file__)
+
+def install():
+	# Your install steps go here
+
+def main():
+	install()
+
+# make things happen
+main()
