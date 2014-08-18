472fbd5e4419f700c28b0e2745a4fd95123239ec
Stuart Herbert <stuart@stuartherbert.com>
2014-08-18 20:35:27 +0100

    Fix: dsf.core.vagrant.get_ip_address() should parse the output line by line

--- core/vagrant.py
+++ core/vagrant.py
@@ -7,6 +7,9 @@ def start(cwd=None, destroy_first=False, vms = ['default']):
 		if not dsf.core.fs.has_file('Vagrantfile'):
 			raise RuntimeError("no Vagrantfile found")
 
+		# set the bridge adapter
+		dsf.core.virtualbox.set_bridge_adapter()
+
 		# start vagrant
 		dsf.core.shell.run(["vagrant", "up"] + vms)
 
@@ -36,7 +39,7 @@ def get_ipv4_address(cwd=None, machine="default"):
 		# get a list of interfaces
 		output = dsf.core.shell.get_output_from_command(["vagrant", "ssh", "-c", "/sbin/ifconfig eth1", machine])
 		regex  = re.compile(r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})')
-		for line in output:
+		for line in output[0].split("\n"):
 			matches = regex.search(line)
 			if matches is not None:
 				return matches.group(1)
