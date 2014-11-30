a7801a54039578911ac5566478fb1f1658cd145a
Stuart Herbert <stuart@stuartherbert.com>
2014-11-30 10:24:54 +0000

    New: start to move HTTP out into a networking section

---
+++ net/http.py
@@ -0,0 +1,10 @@
+import urllib
+import dsf
+
+def download_file(url, target_file, cwd=None):
+	with dsf.core.fs.pushd(cwd):
+		# download the file
+		dsf.core.log.log_command_start(["HTTP", "GET", url, "->", target_file])
+		client=urllib.URLopener()
+		client.retrieve(url, target_file)
+		dsf.core.log.log_command_result(0)
