b18ad10cceaef781f754330a4bbd915f13b714f5
Stuart Herbert <stuart@stuartherbert.com>
2015-01-13 20:47:57 +0000

    Fix: dsf.http.download_file() - add optional cache time to avoid constantly downloading files

--- _net/http.py
+++ _net/http.py
@@ -1,8 +1,17 @@
+import os
+import time
 import urllib
+
 import dsf
 
-def download_file(url, target_file, cwd=None):
+def download_file(url, target_file, cwd=None, cache=360):
 	with dsf.core.fs.pushd(cwd):
+		# is the file too new to download?
+		if os.path.isfile(target_file):
+			details = os.stat(target_file)
+			if details.st_mtime > time.time() - cache:
+				return
+
 		# download the file
 		dsf.dslog.log_command_start(["HTTP", "GET", url, "->", target_file])
 		client=urllib.URLopener()
