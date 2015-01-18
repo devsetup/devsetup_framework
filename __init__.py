9f773209170b6acdec03ee82eecc4ce3c6ada0bf
Stuart Herbert <stuart@stuartherbert.com>
2015-01-18 14:50:15 +0000

    New: very simple module for creating users

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from ._ds import *
 from ._fs import *
 from ._log import *
 from ._net import *
+from ._os import *
 from ._pkg import *
 from ._srv import *
 from ._vcs import *
---
+++ _os/user.py
@@ -0,0 +1,26 @@
+# -*- coding:utf8 -*-
+
+'''
+dsf module for working with a MySQL server
+
+@author: Stuart Herbert <stuart@stuartherbert.com>
+
+https://devsetup.systems/dsf-framework/
+'''
+
+import pwd
+import os
+
+import dsf
+
+def create_user(username, groups=None):
+	if not user_exists(username):
+		# create the user
+		dsf.shell.run(["useradd", "-m", username])
+
+
+def user_exists(username):
+	if pwd.getpwnam(username) is None:
+		return False
+
+	return True
